9df394ef50a5ddf1f51ed27c0c79ee2c206ff53e
sp0f <adam.lyjak@gmail.com>
2017-02-28 13:27:41 +0100

    shebang

--- nagios_checks/late_backup.py
+++ nagios_checks/late_backup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import boto3
 from sys import exit
 from time import time, localtime, strftime, strptime, mktime
--- nagios_checks/no_backup_tag.py
+++ nagios_checks/no_backup_tag.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import boto3
 from sys import exit
 
